a93c3f80098120ea1d0a492e70142de9241e7905
Eric Russo <erickfernusg1@gmail.com>
2023-06-12 15:57:46 -0300

    adding Js code to Gallery

--- index.js
+++ index.js
@@ -2,23 +2,53 @@ const d=document,w=window,n=navigator;
 
 
 const $bubbles= d.querySelector(".bubbles");
+const $hexagonGallery= d.querySelector(".hexagon-gallery")
 let bubblesNum= 50;
+
+
+let hexagonColumn=3,
+hexagonRow= 4;
 
 
 
 d.addEventListener("DOMContentLoaded",()=>{
 
 
-    for(let i=0; i < bubblesNum; i++){
+    for(let b=0; b < bubblesNum; b++){
 
 
         const span= d.createElement("span")
         //span.textContent="hola"
-        span.style=`--i:${i * Math.random()}`
+        span.style=`--i:${b * Math.random()}`
         $bubbles.appendChild(span);
         //console.log(i) 
     
     
+    }
+
+
+    for(let h=0; h < hexagonColumn; h++){
+
+        const hexagon= d.createElement("div")
+        hexagon.classList.add("hexagon")
+
+        for(let i= 0; i < hexagonRow; i++){
+
+            const hex= d.createElement("div");
+            hex.classList.add("hex");
+            hexagon.appendChild(hex);
+
+        }
+
+        $hexagonGallery.appendChild(hexagon);
+
+        if(hexagon.childElementCount === hexagonRow){
+
+            hexagonRow --;
+
+        }
+
+
     }
 
     
