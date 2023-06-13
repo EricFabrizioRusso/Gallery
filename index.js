dc1c345792a19652ec3e9b65d8a0eaf1ffec1dff
Eric Russo <erickfernusg1@gmail.com>
2023-06-12 22:32:30 -0300

    Adding modal

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const d=document,w=window,n=navigator;
-
-
 const $bubbles= d.querySelector(".bubbles");
 const $hexagonGallery= d.querySelector(".hexagon-gallery")
 let bubblesNum= 50;
-
-
 let hexagonColumn=3,
-hexagonRow= 4;
+hexagonRow= 3;
+let draws= [
+    //Sketch
+ '/assets/astro.jpg',
+ '/assets/mercenary.jpg',
+ '/assets/warrior.jpg',
+ '/assets/detective.jpg']
 
+ let art3d=[
+    '/assets/road.jpg',
+    '/assets/copCar.jpg',
+    '/assets/room.jpg'
+ ]
+
+ let conceptArt=[
+    '/assets/cabin.jpg',
+    '/assets/oldWest.jpg',
+    '/assets/nightCity.jpg',
+ ]
+
+let mediaQuery= window.matchMedia("(max-width: 768px)");
 
 
 d.addEventListener("DOMContentLoaded",()=>{
@@ -34,22 +49,111 @@ d.addEventListener("DOMContentLoaded",()=>{
 
         for(let i= 0; i < hexagonRow; i++){
 
+
+            
             const hex= d.createElement("div");
             hex.classList.add("hex");
             hexagon.appendChild(hex);
+            const imgHex=d.createElement("img")
+            imgHex.classList.add("hex__img")
 
+         
+
+        
+                if(hexagonRow=== 3 && h!== 2){
+    
+                    imgHex.src= art3d[i]
+                    hex.appendChild(imgHex)
+                        
+    
+                }
+
+                if(hexagonRow === 3 && h === 2){
+
+                    imgHex.src= conceptArt[i]
+                    hex.appendChild(imgHex)
+
+
+                }
+                if(hexagonRow === draws.length){
+        
+                     
+                        imgHex.src= draws[i]
+                        hex.appendChild(imgHex)   
+                                       
+        
+                }
+
+
+                
+
+
+                if(hexagonRow === 5){
+
+                    hexagonRow = conceptArt.length;
+                    imgHex.src= conceptArt[i]
+                    hex.appendChild(imgHex)
+                
+                }
+
+             
+
+
+
+            
+
+      
+
+        
+
+
+           
+
+            
         }
-
+        
         $hexagonGallery.appendChild(hexagon);
 
         if(hexagon.childElementCount === hexagonRow){
 
-            hexagonRow --;
+            hexagonRow  ++;
+
 
         }
 
 
     }
+
+
+const $modal= d.querySelector(".modal");
+const $modalImg= d.querySelector(".modal__img");
+const $boxImg=d.querySelector(".img__boxImg");
+
+    d.addEventListener("click",(e)=>{
+
+       
+        if(e.target.classList.contains("hex__img")){
+
+
+            $modal.style.display="block";
+          console.log("tocaste un hexagono")
+
+            $boxImg.src= e.target.src
+            console.log(e.target)
+
+            return
+
+        }
+
+        if(e.target.classList.contains("modal__img")){
+
+            console.log("cierre modal")
+            $modal.style.display="none";
+
+        }
+
+
+    })
 
     
 
